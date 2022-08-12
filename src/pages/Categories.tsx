e207dea8b33aeabaa130f6c0345b7fb018a5ec46
edonajemini <74864689+edonajemini@users.noreply.github.com>
2022-08-12 13:38:51 +0200

    Fixed the Routes

---
+++ src/pages/Categories.tsx
@@ -0,0 +1,14 @@
+export function Categories() {
+    return (
+      <ul className="categories-container">
+        <li className="categories-container__list">
+          <ul>
+            <li className="jewelery">Jewelery</li>
+            <li className="mans-clothing">Man's Clothing</li>
+            <li className="electronics"> Electronics </li>
+            <li className="womens-clothing">Women's Clothing</li>
+          </ul>
+        </li>
+      </ul>
+    );
+  }
