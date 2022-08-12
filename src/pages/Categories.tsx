2bc56298bb3ae72505638364b58c47e92c0e202b
edonajemini <74864689+edonajemini@users.noreply.github.com>
2022-08-12 13:40:11 +0200

    updates

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -3,10 +3,10 @@ export function Categories() {
       <ul className="categories-container">
         <li className="categories-container__list">
           <ul>
-            <li className="jewelery">Jewelery</li>
-            <li className="mans-clothing">Man's Clothing</li>
-            <li className="electronics"> Electronics </li>
-            <li className="womens-clothing">Women's Clothing</li>
+            <li className="a">a</li>
+            <li className="b">b</li>
+            <li className="c"> c </li>
+            <li className="d">d</li>
           </ul>
         </li>
       </ul>
