10b65eab07d2fd50216bdce1fa6867d83538786e
edonajemini <74864689+edonajemini@users.noreply.github.com>
2022-08-13 15:18:04 +0200

    updates

--- src/pages/AddProducts.tsx
+++ src/pages/AddProducts.tsx
@@ -41,8 +41,12 @@ setNewproducts(productCopy)
   <input className='number-input' type="number" name='price' placeholder='Price?' required></input>
   <input className='url-input' type="url" name="image" placeholder='The Image URL' required></input>
   <button className='addproduct-btn' onClick={()=>{
-    fetch("http://localhost:4000/newproducts/${item.id}",{
-      method: 'DELETE'
+    fetch("http://localhost:4000/newproducts",{
+      method: 'POST',
+      headers: {
+        'content-Type': 'application/json'
+      },
+      body: JSON.stringify()
     })
   }}>
  Post Product
